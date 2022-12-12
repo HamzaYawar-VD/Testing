842dd861be7525ec1b82b92de919fe91f514cc3f
HamzaYawar-VD <118438854+HamzaYawar-VD@users.noreply.github.com>
2022-12-12 20:00:10 +0500

    Update Task6C1.java
    added some print statements

--- src/main/java/TestCases/Task6C1.java
+++ src/main/java/TestCases/Task6C1.java
@@ -10,16 +10,20 @@ public class Task6C1 extends BaseTest {
 
     @Test(description = "designing")
     public static void designingResource()  {
+        System.out.println("starting designing test case");
         designing();
         BeginnerAssertion("Beginner");
         IntermediateAssertion("Intermediate");
         AdvancedAssertion("Advanced");
+        System.out.println("ending designing test case");
     }
 
 
     @Test(description = "designing")
     public static void programmingResource()  {
+        System.out.println("starting programming test case");
         programming();
+        System.out.println("ending designing test case");
     }
 
 
